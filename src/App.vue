4215acb164e680c238d70874949c7fc6a0afc35a
makma <471842@ntti.edu.kh>
2023-12-11 00:26:28 +0700

    ២.៦ ការអនុវត្តប្រូជិកការឌីហ្សាញវេបសាយ (ភាគ២)

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,13 @@
 <template>
-  <div class="bg-white w-3/4">
-    <div>
-      <img src="@/assets/happy.png" alt="" />
+  <div class="bg-white w-3/4 mx-auto mt-10 p-5 rounded-md space-y-2 shadow-md">
+    <div class="w-full">
+      <img class="w-48" src="@/assets/happy.png" alt="" />
+    </div>
+    <div class="w-full">
+      <img class="rounded w-full" src="@/assets/sunset.jpg" alt="" />
     </div>
     <div>
-      <img src="@/assets/sunset.jpg" alt="" />
-    </div>
-    <div>
-      <h1 class="text-green-600 font-semibold font-mono text-lg">
+      <h1 class="text-green-700 font-semibold font-mono text-xl">
         Sunset Destination
       </h1>
       <p class="text-gray-700 tracking-wide leading-6 font-serif">
@@ -15,7 +15,7 @@
         the solitude of the high desert wasteland of Kernville, CA. We visit the
         area every year for a family camping trip.
       </p>
-      <button class="text-yellow-400 bg-green-700 font-mono text-lg">Visit</button>
+      <button class="text-yellow-400 bg-green-700 hover:bg-green-800 font-mono text-lg py-2 px-16 rounded mt-3 hover:text-yellow-300 transform transition-all duration-150 hover:translate-y-1 shadow-md">Visit</button>
     </div>
   </div>
 </template>
