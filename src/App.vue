b4dc3bffa61cd44c3475cbea87dc5a93adcf09e8
makma <471842@ntti.edu.kh>
2023-12-10 23:41:00 +0700

    ២.៤ ការអនុវត្តប្រូជិកការឌីហ្សាញវេបសាយ (ភាគ១)២.៤ ការអនុវត្តប្រូជិកការឌីហ្សាញវេបសាយ (ភាគ១)

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,27 @@
 <template>
-  <h1 class="text-red-500 text-lg border-2 border-blue-500">Tailwindcss</h1>
+  <div class="bg-white w-3/4">
+    <div>
+      <img src="@/assets/happy.png" alt="" />
+    </div>
+    <div>
+      <img src="@/assets/sunset.jpg" alt="" />
+    </div>
+    <div>
+      <h1 class="text-green-600 font-semibold font-mono text-lg">
+        Sunset Destination
+      </h1>
+      <p class="text-gray-700 tracking-wide leading-6 font-serif">
+        Over a hundred miles away from Los Angeles here I am dreaming, enjoying
+        the solitude of the high desert wasteland of Kernville, CA. We visit the
+        area every year for a family camping trip.
+      </p>
+      <button class="text-yellow-400 bg-green-700 font-mono text-lg">Visit</button>
+    </div>
+  </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+export default {};
+</script>
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+<style></style>
