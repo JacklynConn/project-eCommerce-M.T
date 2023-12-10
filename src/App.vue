97d846c84cd5b763b0b4c4e20a1de1f4027dfa41
makma <471842@ntti.edu.kh>
2023-12-10 22:43:11 +0700

    first tailwind.css

--- src/App.vue
+++ src/App.vue
@@ -1,140 +1,26 @@
 <template>
-  <div>
-    <form @submit.prevent="handleSubmit">
-      <h3>Signup Form</h3>
-      <label for="">Username</label>
-      <input type="text" v-model="username" />
-      <label for="">Email</label>
-      <input type="email" v-model="email" />
-      <label for="">Password</label>
-      <input type="password" v-model="password" />
-      <label for="">Role</label>
-      <select v-model="role">
-        <option value="programmer">Programmer</option>
-        <option value="developer">Developer</option>
-        <option value="designer">Designer</option>
-      </select>
-      <label for="">Skill</label>
-      <input type="text" v-model="skill" @keypress.space="handleAddSkill" />
-      <span>{{ skills }}</span>
-      <div>
-        <input type="checkbox" v-model="terms" />
-        <label for="">AGREE TERMS AND CONDITION</label>
-      </div>
-      <button>Submit</button>
-    </form>
-    <p>Username: {{ username }}</p>
-    <p>Email: {{ email }}</p>
-    <p>Password: {{ password }}</p>
-    <p>Role: {{ role }}</p>
-    <p>Skill: {{ skillS }}</p>
-    <p>Terms: {{ terms }}</p>
-  </div>
+  <h1 class="text-red-500 text-lg border-2 border-blue-500">Tailwindcss</h1>
 </template>
 
-<script>
-import { ref } from "vue";
-export default {
-  setup() {
-    const username = ref("");
-    const email = ref("");
-    const password = ref("");
-    const role = ref("");
-    const skill = ref("");
-    const skills = ref([]);
-    const terms = ref(false);
-
-    const handleAddSkill = (e) => {
-      skills.value.push(skill.value);
-      skill.value = "";
-    };
-
-    const handleSubmit = (e) => {
-      e.preventDefault();
-      console.log(`
-        Username: ${username.value}
-        Email: ${email.value}
-        Password: ${password.value}
-        Role: ${role.value}
-        Skill: ${skills.value}
-        Agree Terms: ${terms.value ? "Yes" : "No"}}`
-        );
-    };
-
-    return {
-      username,
-      email,
-      password,
-      role,
-      skill,
-      skills,
-      terms,
-      handleAddSkill,
-      handleSubmit,
-    };
-  },
-};
-</script>
-
 <style>
-div {
-  width: 900px;
-  margin: 30px auto;
-}
-p {
-  font-size: 30px;
-  color: gray;
-  font-style: italic;
-}
-form {
-  max-width: 600px;
-  background-color: white;
-  padding: 40px;
-  border-radius: 10px;
-  text-align: left;
-}
-h3 {
-  font-size: 40px;
-  color: blue;
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
   text-align: center;
-}
-label {
-  display: inline-block;
-  color: #aaa;
-  font-size: 20px;
-  margin: 25px 0 15px;
-  letter-spacing: 1px;
-  text-transform: uppercase;
-}
-input, select {
-  display: block;
-  color: #aaa;
-  padding: 10px 6px;
-  width: 100%;
-  border: none;
-  border-bottom: 1px solid #ddd;
-  font-size: 25px;
-}
-span{
-  margin: 10px 0;
-  font-size: 30px;
-  color: blue;
-}
-input[type="checkbox"]{
-  display: inline-block;
-  margin: 0 20px 0 0;
-  width: 20px;
-
-}
-button{
-  display: block;
-  margin: 20px 0 0;
-  padding: 10px 20px;
-  background-color: blue;
-  color: white;
-  font-size: 25px;
-  border: none;
-  border-radius: 5px;
+  color: #2c3e50;
 }
 
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
 </style>
---
+++ src/views/Employee.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <ul v-for="staff in employee" :key="staff.id">
+      <router-link :to="{ name: 'staff', params: { id: staff.id } }">
+        <li>{{ staff.name }}</li>
+      </router-link>
+    </ul>
+  </div>
+</template>
+<script>
+import { ref } from "@vue/reactivity";
+export default {
+  setup() {
+    const employee = ref([
+      {
+        id: "emp_001",
+        name: "Bopha",
+      },
+      {
+        id: "emp_002",
+        name: "Rothana",
+      },
+      {
+        id: "emp_003",
+        name: "Sokha",
+      },
+      {
+        id: "emp_004",
+        name: "Sokchea",
+      },
+      {
+        id: "emp_005",
+        name: "Sokly",
+      }
+    ]);
+    return { employee };
+  },
+};
+</script>
+<style></style>
